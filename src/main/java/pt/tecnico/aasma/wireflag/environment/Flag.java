e5571189d08946c69c1026320b04a049a728e498
David Duarte <david.duarte@ist.utl.pt>
2014-04-02 00:46:39 +0100

    Migrate from Ant to Maven and moved. Closes #1 by moving the code to src/main/java and replaces the package to pt.tecnico.aasma.wireflag

---
+++ src/main/java/pt/tecnico/aasma/wireflag/environment/Flag.java
@@ -0,0 +1,44 @@
+package pt.tecnico.aasma.wireflag.environment;
+
+import org.newdawn.slick.Animation;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
+import pt.tecnico.aasma.wireflag.GameElement;
+
+public class Flag implements GameElement {
+
+	private Animation flag;
+	private int xCoord;
+	private int yCoord;
+
+	public Flag() {
+
+	}
+
+	public void update() {
+
+	}
+
+	@Override
+	public void init() throws SlickException {
+
+		flag = new Animation(new Image[] { new Image("data/SmallFlag.png") },
+				new int[] { 300 }, false);
+
+	}
+
+	@Override
+	public void render(Graphics g) {
+
+		flag.draw(xCoord * 1.0f, yCoord * 1.0f);
+
+	}
+
+	public void setCoords(int xCoord, int yCoord) {
+		this.xCoord = xCoord;
+		this.yCoord = yCoord;
+	}
+
+}
