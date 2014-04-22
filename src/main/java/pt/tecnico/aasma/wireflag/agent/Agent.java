c3a0a07ce78d2b1ec442392066c181d946c8ffc1
JMCC <jose.cavalheiro@tecnico.ulisboa.pt>
2014-04-22 22:54:35 +0100

    Debug setting was added

--- src/main/java/pt/tecnico/aasma/wireflag/agent/Agent.java
+++ src/main/java/pt/tecnico/aasma/wireflag/agent/Agent.java
@@ -133,6 +133,10 @@ public abstract class Agent implements IGameElement {
 		return visibility;
 	}
 
+	public Architecture getArch() {
+		return architecture;
+	}
+
 	/***************
 	 *** SETTERS ***
 	 ***************/
